34513b78fb88a73505c73462d8e908fa60499caf
wrolson96 <wade.olson.j@gmail.com>
2024-02-21 10:29:21 -0700

    HTML outline

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("My code is running!")
+
+
+var list = [item1, item2, item3]
+// Users should be able to view all of the to-do tasks
+// Users should be able to add a to-do to the list of to-do tasks
+// Users should be able to mark a task as completed by crossing it out on click
+// Users should be able to remove to-do tasks by removing those elements from the DOM
+// The page should be aesthetically pleasing (aka be styled)
